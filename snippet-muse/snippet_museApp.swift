0bac95edefb61eb80dfcb6aed00451ca580563b7
Claire Xu <claireaxu@g.ucla.edu>
2024-10-12 18:29:41 -0700

    Initial Commit

---
+++ snippet-muse/snippet_museApp.swift
@@ -0,0 +1,17 @@
+//
+//  snippet_museApp.swift
+//  snippet-muse
+//
+//  Created by Claire Xu on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct snippet_museApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ snippet-museTests/snippet_museTests.swift
@@ -0,0 +1,17 @@
+//
+//  snippet_museTests.swift
+//  snippet-museTests
+//
+//  Created by Claire Xu on 10/12/24.
+//
+
+import Testing
+@testable import snippet_muse
+
+struct snippet_museTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
